a3936bd2f567e494c0df3b119c48476ab31d657a
Romisha08 <romisha03@gmail.com>
2025-02-14 13:12:47 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,75 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript/C++ these days.  
+I've made some games in that language. I also like to make websites.  
+Anyway, regardless of the language, I just like programming.  
+I also like to learn new things. I'm currently learning Golang.  
+<br> The Frameworks & Libraries I use are:
+
+HTML, CSS, Bootstrap, Tailwind CSS, ReactJS, Astro, Next.js  
+
+
+<br> Here is my github link .Go check out my other projects!! <a href="https://github.com/Romisha08" target="_blank">Github</a>!!`,
+
+  "cs-degree": `I'm an Electronics and Communication Engineering 3rd-year student.  
+Gonna hang the degree here later, but I also have knowledge of CS fundamentals like:  
+  
+Data Structures & Algorithms, DBMS, MySQL, Computer Networking, OOPs, Operating Systems`,
+  "sofa-table": `That's my sofa. I like to relax here and watch TV sometimes to unwind. Meanwhile, you guys can check out my achievements and certificates for different courses I've completed or taken so far:
+
+
+<br> 1. <a href="certificate_link_1" target="_blank">Microsoft Copilot Studio</a> – Brief description (e.g., Web Development, Data Science, etc.)     
+
+<br> 2. <a href="certificate_link_2" target="_blank">Excel Dashboard and Automation using ChatGpt</a> – Brief description (e.g., Advanced JavaScript, Machine Learning, etc.)  
+
+`,
+
+  tv: `That's my TV. Instead of watching shows, I often check out different platforms where I share my work and solve coding challenges. You can find me here:  
+
+<br> <a href="your_linkedin_url" target="_blank">LinkedIn</a>  
+<br> <a href="your_github_url" target="_blank">GitHub</a>  
+<br> <a href="your_leetcode_url" target="_blank">LeetCode</a>  
+<br> <a href="your_coding_ninja_url" target="_blank">Coding Ninjas</a>  
+<br> <a href="your_codeforces_url" target="_blank">Codeforces</a>  
+
+ <br>Feel free to connect with me!`,
+
+  bed: `This is where I sleep—my ultimate reset button. No matter how the day goes, a good nap or deep sleep makes everything feel better. It’s the one place where I can truly relax and recharge.`,
+  resume: `This is my desk and on it is my resume. <a href="#" target="_blank">Check it out?</a>
+  Contact me if you have any interesting opportunities!`,
+  projects: `Info about this portfolio: It's made with the Kaboom.js library, which is used for making games in JavaScript.  
+It also uses tiles for rendering the environment and is built with Vite for fast development and bundling.  
+Text is rendered with HTML/CSS, meaning the textbox you're currently reading is not rendered within the canvas.  
+
+The process included designing maps using Tiled, implementing player movement, handling collisions,  
+displaying dialogue, scaling the camera, and adding animations for a polished experience.`,
+  library: ` I enjoy building things, especially **interactive & innovative projects**.  
+Here are some of my recent works:
+
+<br>Ai-Logo-Generator 
+  <a href="https://github.com/Romisha08/Ai-Logo-Generator" target="_blank">🔗 Code</a> |  
+  <a href="https://drive.google.com/file/d/19aSkBAEUc3kUVpJuCKc6XPHySddJJjuf/view?usp=drive_link" target="_blank">🎮 View Project</a>
+
+<br> Ecpedia 
+  <a href="https://github.com/Romisha08/ECpedia_wesite" target="_blank">🔗 Code</a> |  
+  <a href="https://ecpedia-website.netlify.app/" target="_blank">📱 View Project</a>
+
+<br>Flappy-Bird-Game  
+  <a href="https://github.com/Romisha08/Classic-Flappy-Bird-Game" target="_blank">🔗 Code</a> |  
+  <a href="https://classic-flappy-bird-game.vercel.app/" target="_blank">🩺 View Project</a>
+
+<br>Playlist Timer
+  <a href="https://github.com/Romisha08/youtube-playlist-timer" target="_blank">🔗 Code</a> |  
+  <a href="https://youtube-playlist-timer.vercel.app/" target="_blank">📚 View Project</a>
+
+<br>FlowForge(Ongoing) 
+  <a href="https://github.com/Romisha08/flowforge" target="_blank">🔗 Code</a> |  
+  <a href="" target="_blank">📚 View Project</a>
+More projects are coming soon! Stay tuned. 🚀
+    
+  <br>  If you want to reach out, feel free to email me at <a href="mailto:romisha08b@gmail.com">romisha08b@gmail.com</a>.`,
+  exit: `If you want to exit Romisha's portfolio, just close the tab.`,
+};
+
+// export const scaleFactor = 3;
